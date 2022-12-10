a97a81c327602be0be3d8899efbfda4a2cb82799
jsalexsandro <eujosealexsandro@gmail.com>
2022-12-10 14:54:15 -0300

    Adjusted bug

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -5,10 +5,11 @@ interface CheckboxProps {
   id:string
 }
 
+
 export function CheckBox(props:CheckboxProps){
   return  (
-    <div className="w-[320px] flex items-center justify-start py-2">
-      <MaterialCheckBox className="m-0 p-0 w-auto h-auto" {...props} defaultChecked size="medium" />
+    <div className="w-[320px] flex items-center justify-start py-1">
+      <MaterialCheckBox style={{ width:"auto", height:"auto",padding:"0"  }} {...props} defaultChecked size="medium" />
       <label className="px-2" htmlFor={props.id}>
         { props.label }
       </label>
